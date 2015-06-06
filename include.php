4aaa597e183c10f750bf22c2a8a8ad845d3f5e6d
zsx <zsx@zsxsoft.com>
2015-06-06 19:23:38 +0800

    Create Plugin

---
+++ include.php
@@ -0,0 +1,7 @@
+<?php
+#注册插件
+RegisterPlugin("themeEditor","ActivePlugin_themeEditor");
+
+function ActivePlugin_themeEditor() {}
+function InstallPlugin_themeEditor() {}
+function UninstallPlugin_themeEditor() {}
